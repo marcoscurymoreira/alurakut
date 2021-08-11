e8f6f6b0f411d060791e91c0b098c2dc36cf2ef9
Marcos Cury Moreira <quinhopd@gmail.com>
2021-08-10 21:40:06 -0300

    updating front

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src="https://github.com/marcoscurymoreira.png" />
+        </Box>
+      </div>
+
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Pessoas da comunidade
+        </Box>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+
+    </MainGrid>
+  )
 }
